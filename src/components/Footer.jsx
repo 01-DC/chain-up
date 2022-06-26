a5b794089d7ae9e7a1f3668263ad85370252280e
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-06-26 16:55:08 +0530

    changed Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,28 +1,19 @@
 import React from "react"
 
+import { AiOutlineStar } from "react-icons/ai"
+
 export default function Footer() {
 	return (
 		<footer className="bg-neutral text-neutral-content mt-4">
 			<div className="container mx-auto footer py-10">
-				<div>
-					<span className="footer-title">Services</span>
-					<a className="link link-hover">Branding</a>
-					<a className="link link-hover">Design</a>
-					<a className="link link-hover">Marketing</a>
-					<a className="link link-hover">Advertisement</a>
-				</div>
-				<div>
-					<span className="footer-title">Company</span>
-					<a className="link link-hover">About us</a>
-					<a className="link link-hover">Contact</a>
-					<a className="link link-hover">Jobs</a>
-					<a className="link link-hover">Press kit</a>
-				</div>
-				<div>
-					<span className="footer-title">Legal</span>
-					<a className="link link-hover">Terms of use</a>
-					<a className="link link-hover">Privacy policy</a>
-					<a className="link link-hover">Cookie policy</a>
+				<div className="flex justify-between items-center w-full">
+					<p>{"Made with <3 by DC and Niroj"}</p>
+					<a href="https://github.com/01-DC/chain-up">
+						<button class="btn gap-2">
+							<AiOutlineStar style={{ fontSize: "24px" }} />
+							Github
+						</button>
+					</a>
 				</div>
 			</div>
 		</footer>
